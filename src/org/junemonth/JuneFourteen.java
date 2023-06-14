8126ea047e1791857127057e9161461980f248ff
unknown <gayathrisureshkumar1222@gmail.com>
2023-06-14 14:56:09 +0530

    aus

--- src/org/junemonth/JuneFourteen.java
+++ src/org/junemonth/JuneFourteen.java
@@ -1,9 +1,15 @@
+
 package org.junemonth;
 
 public class JuneFourteen {
 
 	private void vacation() {
 		System.out.println("Go to vaction");
+
+	}
+	
+	private void foreien() {
+		System.out.println("Go to Australia");
 
 	}
 }
